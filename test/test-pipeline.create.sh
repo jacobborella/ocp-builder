5bf585711e08a5718c0a15031e1c66c17f59819a
Jacob Borella <jacobborella@hotmail.com>
2017-09-15 16:22:39 +0200

    added creation of test project script

--- test/test-pipeline.create.sh
+++ test/test-pipeline.create.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-oc whoami
+oc new-build --image-stream=openjdk18-openshift --binary=true --name=hello-world
+oc start-build hello-world --from-dir=.
+oc new-app hello-world
+oc expose svc hello-world
