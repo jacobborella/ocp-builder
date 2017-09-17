eeb9a3d348ad0a633cef9b5c51be18e35f04a666
Jacob Borella <jacobborella@hotmail.com>
2017-09-17 23:23:32 +0200

    Create script for test project

--- test/test-pipeline.create.sh
+++ test/test-pipeline.create.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-oc new-app openjdk18-openshift~https://github.com/jacobborella/myproject --name hello-world -e GREETING=Hello GOTO
+oc new-app openjdk18-openshift~https://github.com/jacobborella/myproject --name hello-world -e GREETING="Hello GOTO"
 oc expose svc hello-world
